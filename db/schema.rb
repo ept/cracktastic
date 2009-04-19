503f3559ffa06e0b5aaa0722c0272c01c2019cda
Martin Kleppmann <martin@eptcomputing.com>
2009-04-20 00:42:47 +0100

    Migrated to invoicing_ledger schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090419233848) do
 
   create_table "companies", :force => true do |t|
     t.boolean  "is_self"
@@ -29,6 +29,38 @@ ActiveRecord::Schema.define(:version => 0) do
   create_table "jokes", :force => true do |t|
     t.text     "question"
     t.text     "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ledger_items", :force => true do |t|
+    t.string   "type"
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "issue_date"
+    t.string   "currency",     :limit => 3,                                 :default => "GBP", :null => false
+    t.decimal  "total_amount",               :precision => 20, :scale => 4
+    t.decimal  "tax_amount",                 :precision => 20, :scale => 4
+    t.string   "status",       :limit => 20
+    t.string   "description"
+    t.datetime "period_start"
+    t.datetime "period_end"
+    t.string   "uuid",         :limit => 40
+    t.datetime "due_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.string   "type"
+    t.integer  "ledger_item_id"
+    t.decimal  "net_amount",                   :precision => 20, :scale => 4
+    t.decimal  "tax_amount",                   :precision => 20, :scale => 4
+    t.string   "description"
+    t.string   "uuid",           :limit => 40
+    t.datetime "tax_point"
+    t.decimal  "quantity",                     :precision => 20, :scale => 4
+    t.integer  "creator_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
